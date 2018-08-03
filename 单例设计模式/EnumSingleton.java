e662d4d0c71425f252d38399575ea789429eabb1
CR9C <CR752099@163.com>
2018-08-03 10:40:46 +0800

    添加了枚举方式

---
+++ 单例设计模式/EnumSingleton.java
@@ -0,0 +1,12 @@
+package io.github.cr9c;
+
+/**
+ * 如果枚举值只有 1 个的时候，其实例也就只有 1 个，就完全符合了单例模式的限定。
+ * 由于 Java 的 enum 可以跟类一样任意定义方法和属性，所以就完全可以用枚举来实现单例。
+ * 代码中INSTANCE本身就是一个EnumSingleton类型的引用, 与普通的饿汉式单例相似。
+ */
+enum EnumSingleton {
+	INSTANCE;
+	public void doSomeThing() {
+	}
+}
